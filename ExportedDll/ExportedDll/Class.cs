bd85cbb7c77218b568c9f125f169b2af5ddb32ea
Ilya Baryko <38408304+shineRR@users.noreply.github.com>
2020-11-13 13:54:56 +0300

    Added DLL

---
+++ ExportedDll/ExportedDll/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExportedDll
+{
+    public class Class
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void Bye()
+        {
+            Console.WriteLine("Bye");
+        }
+
+        private static void Yes()
+        {
+            Console.WriteLine("static");
+        }
+        
+        private void Hey()
+        {
+            Console.WriteLine("Hey");
+        }
+    }
+}
